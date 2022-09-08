3f269c81976b9955275b6b02f90f25cac7d269f2
Daniel <daniel.xds@hotmail.com>
2022-09-08 18:20:29 -0300

    arquivo para exportar dados para excel com linguagem python

---
+++ export_excel/export.py
@@ -0,0 +1,16 @@
+#    para a exportação para Microsof Excel estou usando o modulo pandas 
+#    para instalar abra o prompt de comando do windows e digite: pip install pandas
+
+import pandas as pd
+
+# criação do DataFrame
+dados = pd.DataFrame({'Codigo': [1, 2, 3, 4, 5, 6], 
+    'Nome': ['Daniel', 'Vilma', 'Isabella', 'Denis', 'Edna', 'Maria'], 
+    'Instagram': ['daniel.xds93', 'vilmanunes104', 'xaviier_13', 'aprendendo.doinicio', 
+    'edna.apmacedo', 'aprendendo.doinicio']})
+
+# a linha abaixo crio o nome do arquivo .xlsx
+nome_xlsx = 'usuarios_dados.xlsx'
+
+# a linha abaixo seta a exportação para excel, passando o nome do arquivo, indexação e cabeçalho
+dados.to_excel(nome_xlsx, index=False, header=True)
