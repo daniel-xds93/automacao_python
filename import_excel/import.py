89d08a579c5acaab3fab8de137e52a9e415528c0
Daniel <daniel.xds@hotmail.com>
2022-09-09 16:17:44 -0300

    finalização do projeto em python que faz leitura de arquivo excel .xlsx

--- import_excel/import.py
+++ import_excel/import.py
@@ -6,8 +6,12 @@
 
 import pandas as pd
 
-nomeXlsx = ""
+# a linha abaixo é a variavel que recebe o caminho e nome do arquivo a ser importado
+nomeXlsx = "import_excel/arquivo.xlsx"
 
+# a linha abaixo é a variavel que recebe as linhas e colunas do arquivo excel
+# pd.read_excel() -> faz a leitura do arquivo 
 dados = pd.read_excel(nomeXlsx, engine="openpyxl")
 
+# aqui eu apenas mostro em tela os dados do arquivo
 print(dados)
