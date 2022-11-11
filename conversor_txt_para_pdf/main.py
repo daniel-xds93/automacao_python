98cd9a167816c0ae5da33d2370697c1a9f3cd2c2
Daniel <daniel.xds@hotmail.com>
2022-11-11 11:42:16 -0300

    ajuste para arquivos q tem pontos no nome

--- conversor_txt_para_pdf/main.py
+++ conversor_txt_para_pdf/main.py
@@ -5,12 +5,13 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 
-
 class Tela:
     def __init__(self):
         
         self.layout = Tk()
         
+        self.layout.title("Conversor")
+        self.layout.geometry("250x80")
         self.parteCima = Frame(self.layout)
         self.parteBaixo = Frame(self.layout)
 
@@ -22,10 +23,11 @@ class Tela:
         self.parteBaixo.pack()
         self.txtInformativo.pack(anchor = "w")
         self.btnSelPasta.pack(anchor = "w")
-
+        
         mainloop()
 
         # janela = self.Window("Conversor", layout)
+
     def executar(self):
         
         try:
@@ -33,32 +35,38 @@ class Tela:
             caminho_pasta = path.join(pasta)
             lista_arquivos = listdir(pasta)
 
-            def converter_para_pdf(file, nomePDF):
+            def converter_para_pdf(file, nomeArquivo):
 
                 pdf = FPDF()
                 pdf.add_page()
 
                 arquivo_res = file
+                arqPDF = ""
                         
                 for text in arquivo_res:
-                    pdf.set_font("Arial",size=12) # For paragraph text
+                    pdf.set_font("Arial",size=10) # For paragraph text
                     pdf.cell(w=0,h=10,txt=f'{text}', ln=1, align="L")
                     
-                nomePDF = caminho_pasta + "/" + nomePDF + '.pdf'
-                
+                for i in range(len(nomeArquivo)-4):
+                    arqPDF += nomeArquivo[i]
+
+                nomePDF = caminho_pasta + "/" + arqPDF + '.pdf'
+
                 pdf.output(nomePDF)   
 
-            if lista_arquivos and caminho_pasta:
+            if lista_arquivos and caminho_pasta:                
                 for item in range(len(lista_arquivos)):
                     arquivo = lista_arquivos[item]
                     caminho_nome = caminho_pasta + "/" + arquivo
                     nomePDF = arquivo.split('.')
+                    
                     if len(nomePDF) <= 1 or nomePDF[len(nomePDF)-1] != "txt":
                         continue
-                    nomePDF = nomePDF[0]
+                    
                     file = open(caminho_nome, "r")
-                    converter_para_pdf(file, nomePDF)
-            
+                    converter_para_pdf(file, arquivo)
+                    file.close()
+
                 messagebox.showinfo('Concluído', "Conversão concluída")
             else:
                 messagebox.showerror('Atenção','Pasta informada, não possuem arquivos que possam ser convertidos.')
